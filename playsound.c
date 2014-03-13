aad2ae42526f3198bbaedafcf8c61cfb84091846
renan <renan@renan-vm-ubuntu>
2014-03-12 21:51:54 -0400

    starting to work in to play a sound.

--- playsound.c
+++ playsound.c
@@ -338,14 +338,77 @@ void test_record() {
 	snd_pcm_drain(handle);
 	snd_pcm_close(handle);
 	free(buffer);
+}
 
+snd_pcm_t* open_audio() {
+	int rc;
+	snd_pcm_t *handle;
+	snd_pcm_hw_params_t *params;
+	unsigned int val, val2;
+	int dir;
+	snd_pcm_uframes_t frames;
+
+	/* Open PCM device for playback. */
+	rc = snd_pcm_open(&handle, "plughw:", SND_PCM_STREAM_PLAYBACK, 0);
+	if (rc < 0) {
+		fprintf(stderr, "unable to open pcm device: %s\n", snd_strerror(rc));
+		exit(1);
+	}
+
+	/* Allocate a hardware parameters object. */
+	snd_pcm_hw_params_alloca(&params);
+
+	/* Fill it in with default values. */
+	snd_pcm_hw_params_any(handle, params);
+
+	/* Set the desired hardware parameters. */
+
+	/* Interleaved mode */
+	snd_pcm_hw_params_set_access(handle, params, SND_PCM_ACCESS_RW_INTERLEAVED);
+
+	/* Signed 16-bit little-endian format */
+	snd_pcm_hw_params_set_format(handle, params, SND_PCM_FORMAT_S16_LE);
+
+	/* Two channels (stereo) */
+	snd_pcm_hw_params_set_channels(handle, params, 2);
+
+	/* 44100 bits/second sampling rate (CD quality) */
+	val = 44100;
+	snd_pcm_hw_params_set_rate_near(handle, params, &val, &dir);
+
+	/* Write the parameters to the driver */
+	rc = snd_pcm_hw_params(handle, params);
+	if (rc < 0) {
+		fprintf(stderr, "unable to set hw parameters: %s\n", snd_strerror(rc));
+		exit(1);
+	}
+	printf("audio opened successfully\n");
+	return handle;
+}
+
+void play_note() {
+	printf("playing note....\n");
+}
+
+void close_audio(snd_pcm_t *handle) {
+	snd_pcm_close(handle);
+	printf("audio close successfully\n");
 }
 
 int main(int argc, char *argv[]) {
+	snd_pcm_t* handle;
+
+	handle = open_audio();
+
+	play_note();
+
+	close_audio(handle);
+
+
 	//print_asound_info();
 	//test_open();
-	//test_sound();
-	test_record();
+	// test_sound();
+	//test_record();
 	return 0;
 }
 
